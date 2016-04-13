c328bf4c90c638945bfb66aec4d225846b1a825a
Nghia Tran <vinhnghiatran@gmail.com>
2016-04-13 10:07:36 +0000

    Added Test for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = User.new(name: "Nghia Tran", email: "vinhnghiatran@gmail.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "should be presence" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
 end
