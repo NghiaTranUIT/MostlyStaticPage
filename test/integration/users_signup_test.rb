d788b5bcdd64004c5e633ee3665b2115cb6b28b5
Nghia Tran <vinhnghiatran@gmail.com>
2016-04-28 16:10:47 +0000

    Finished 7.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     
     assert_template 'users/new'
   end
+  
+  test "valid signup information" do
+    get signup_path
+    
+    assert_difference 'User.count', 1 do 
+      
+      post users_path, user: {name:  "nghiatran123",
+                               email: "vinhnghia@gmail.com",
+                               password:              "thenotebook",
+                               password_confirmation: "thenotebook"}
+                               
+      end
+      
+      assert_template 'users/show'
+  end
 end
